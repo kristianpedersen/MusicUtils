8a5e6fe0c4e69f50ab1260f3e7c2e474510a1585
kristianpedersen <kristianp89@gmail.com>
2020-04-19 12:42:45 +0200

    Implemented unison

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// These are the waveforms that work in Chrome
 enum Waveform {
     triangle = 1,
     sawtooth = 2,
@@ -10,15 +11,15 @@ enum Waveform {
     square50 = 15,
 }
 
-type SoundAttribute = {
-    wave: Waveform;
-    frequency?: number;
-    duration: number;
-    endFrequency?: number;
-    /** @default 100 */
-    startVolume?: number;
-    /** @default 0 */
-    endVolume?: number;
+type SoundAttributes = {
+    wave: Waveform
+    frequency?: number // Not optional, but handled by its own function
+    duration: number
+    endFrequency?: number
+    startVolume?: number
+    endVolume?: number
+    ID?: number // We use the note's frequency as lookup
+    detune?: number
 }
 
 namespace music {
@@ -26,29 +27,43 @@ namespace music {
     export function queuePlayInstructions(timeDelta: number, buf: Buffer) { }
 }
 
-class Sound {
+class Track {
     generatedNotes: Buffer[]
+    melody: number[]
 
-    constructor(melody: number[], settings: SoundAttribute) {
-        this.generatedNotes = makeUnique(melody).map(note => this.createNote({
-            wave: settings.wave,
-            frequency: note * (1 + Math.random() * 0.01),
-            duration: settings.duration
-        }))
+    constructor(melody: number[], settings: SoundAttributes) {
+        this.melody = melody
+        this.generatedNotes = getUniqueNotesFrom(melody)
+            .map(note => this.createNote({
+                wave: settings.wave,
+                frequency: note * (1 + Math.random() * (settings.detune || 0)),
+                duration: settings.duration,
+                startVolume: settings.startVolume || 100,
+                endVolume: settings.endVolume || 0,
+                ID: note,                
+            }))
     }
 
-    playNotes() {
-        this.generatedNotes.forEach((note, index) => music.queuePlayInstructions(0, note))
+    playNotesWithInterval(intervalInMS: number) {
+        this.melody.forEach((note, index) => {
+
+        })
+       
+        this.generatedNotes.forEach((note, index) => {
+            music.queuePlayInstructions(index * intervalInMS, note)
+        })
+         
+
     }
 
-    createNote(s: SoundAttribute) {
+    createNote(s: SoundAttributes) {
         const individualNote = control.createBuffer(96);
         individualNote.setNumber(NumberFormat.UInt8LE, 0, s.wave);
         individualNote.setNumber(NumberFormat.UInt8LE, 1, 0); // Unused (https://arcade.makecode.com/developer/sound)
         individualNote.setNumber(NumberFormat.UInt16LE, 2, s.frequency);
         individualNote.setNumber(NumberFormat.UInt16LE, 4, s.duration);
-        individualNote.setNumber(NumberFormat.UInt16LE, 6, s.startVolume || 100);
-        individualNote.setNumber(NumberFormat.UInt16LE, 8, s.endVolume || 0);
+        individualNote.setNumber(NumberFormat.UInt16LE, 6, s.startVolume);
+        individualNote.setNumber(NumberFormat.UInt16LE, 8, s.endVolume);
         individualNote.setNumber(NumberFormat.UInt16LE, 10, s.frequency);
 
         if (s.endFrequency) {
@@ -59,14 +74,19 @@ class Sound {
     }
 }
 
-const tune = new Sound([Note.D3 * 0.5, Note.A3, Note.C, Note.D, Note.FSharp, Note.G], {wave: Waveform.triangle, duration: 2000})
-tune.playNotes()
+const myMelody = [Note.D3 * 0.5, Note.FSharp3, Note.A3, Note.FSharp3, Note.FSharp4, Note.A4, Note.FSharp4]
+const mySound = { wave: Waveform.square40, duration: 5000, startVolume: 20, detune: 0.02 }
 
-// Utility function used to generate arrays with unique values
-// [1, 2, 3, 1] -> [1, 2, 3]
-function makeUnique(array: number[]) {
+const track1 = new Track(myMelody, mySound)
+track1.playNotesWithInterval(100)
+
+/**
+ * Return array without duplicates.
+ * MakeCode requires each individual note in a melody to be generated as a buffer.
+ */
+function getUniqueNotesFrom(array: number[]) {
     return array.reduce((accumulator, currentValue) => {
-        if (!accumulator.some(element => element == currentValue)) {
+        if (accumulator.every(element => element != currentValue)) {
             accumulator.push(currentValue)
         }
         return accumulator
